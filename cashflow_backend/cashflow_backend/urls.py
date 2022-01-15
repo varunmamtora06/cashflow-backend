5f9256449cb46e80c4a1d9991fc721fbe927e295
Varun Mamtora <varunmamtora@gmail.com>
2022-01-15 11:35:42 +0530

    setup static and media routes

--- cashflow_backend/cashflow_backend/urls.py
+++ cashflow_backend/cashflow_backend/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include('main.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
