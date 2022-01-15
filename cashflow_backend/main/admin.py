586d85d20b3e3d7c703b315ca576ba89282ffafc
Varun Mamtora <varunmamtora@gmail.com>
2022-01-15 13:36:45 +0530

    register models

--- cashflow_backend/main/admin.py
+++ cashflow_backend/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Category, Expenditure, ExpenditureReceipt, Reminder, Goal
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Expenditure)
+admin.site.register(ExpenditureReceipt)
+admin.site.register(Reminder)
+admin.site.register(Goal)
