a68a3fe7f8aa1d583afb61568c7375b1bc527413
Varun Mamtora <varunmamtora@gmail.com>
2022-01-18 14:28:35 +0530

    extend expire time

--- cashflow_backend/cashflow_backend/settings.py
+++ cashflow_backend/cashflow_backend/settings.py
@@ -172,7 +172,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=3),
 
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
